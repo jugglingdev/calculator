9f562cf9574b17784e3e9c3e481a108c0ba6376c
jugglingdev <kaylamarie1785@gmail.com>
2023-04-29 23:27:57 -0500

    Initial JS file with pseudocode

---
+++ myscript.js
@@ -0,0 +1,32 @@
+// add
+// subtract
+// multiply
+// divide
+
+// variables:  num1 operator num2
+// also: displayValue
+
+// function operate(num1 operator num2)
+
+// function reset()
+
+// function updateDisplay() with solution
+
+// hardest part is store all the values and call the operate() function with them
+
+// be able to string together several operations (12 + 7 - 5 * 3 = 42) => 
+// calculator should first evaluate the first pair of numbers (12 + 7), second display the result (19), and last use the result (19) as the first number in the new calculation along with the next operator (-)
+
+// round answers with long decimals so they don't overflow the screen
+
+//pressing = before entering all the numbers or an operator could cause problems!
+
+// pressing clear/reset should wipe out any existing data
+
+// display an error message if the user tries to divide by 0
+
+// don't allow more than one decimal (12.3.56..5)
+
+// backspace/delete for undoing a number
+
+// double check / support - check MDN event.preventDefault
